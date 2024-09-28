30da17bef31544906eca5f0091aacbbb0a508298
goldenjeffempire <jeffemuodafe124@gmail.com>
2024-09-28 05:12:11 +0100

    Add Comment model

--- ThoughtStream/models.py
+++ ThoughtStream/models.py
@@ -11,3 +11,12 @@ class BlogPost(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(BlogPost, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.post}'
